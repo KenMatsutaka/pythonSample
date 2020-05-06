ecda4f157ac5e438369e696989a29be835bd83fc
KenMatsutaka <k.matutaka.biz@gmail.com>
2020-05-06 16:15:27 +0900

    loggerサンプル作成完了

---
+++ sample13.py
@@ -0,0 +1,10 @@
+# logging : ログ出力を扱う為のライブラリ
+import logging
+
+# ログの設定
+logging.basicConfig(filename="sampleLog.log", level=logging.WARN)
+logging.fatal("fatal")
+logging.error("error")
+logging.warning("warn")
+logging.info("into")
+logging.debug("debug")
